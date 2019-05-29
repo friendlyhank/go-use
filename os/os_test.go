a730ee1ec6980ddd0ca4ef647837b37cf87b8055
a853160447 <hanguang8888@outlook.com>
2019-05-29 15:33:21 +0800

    os

--- os/os_test.go
+++ os/os_test.go
@@ -1,21 +1,22 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"github.com/astaxie/beego/logs"
 	"os"
 	"testing"
+	"errors"
 )
+
+//=============================================os base=======================================
 
 //打开文件
 func TestOsOpen(t *testing.T){
 	file, err := os.Open("../test.txt")
 	if err != nil {
-		logs.Info("%v",err)
+		t.Errorf("%v",err)
+		return
 	}
-	logs.Info("%v", file)
-
+	t.Logf("%v",file.Name())
 	//open file: no such file or directory
 }
 
@@ -23,6 +24,10 @@ func TestOsOpen(t *testing.T){
 func TestOsSetenv(t *testing.T){
 	//设置环境变量(并没有设置到环境变量中,应该是临时的)
 	err := os.Setenv("TMPDOR","D:/logtest/")
+	if err != nil{
+		t.Errorf("%v",err)
+		return
+	}
 
 	//获取环境变量
 	show := func(key string){
@@ -40,8 +45,20 @@ func TestOsSetenv(t *testing.T){
 
 	//删除环境变量
 	err = os.Unsetenv("TMPDOR")
-	logs.Info("%v",err)
+	if err != nil{
+		t.Errorf("%v",err)
+		return
+	}
+}
 
+//TestOsHostName - 返回主机名
+func TestOsHostName(t *testing.T){
+	hostname,err := os.Hostname()
+	if err != nil{
+		t.Errorf("%v",err)
+		return
+	}
+	t.Logf("%v",hostname)
 }
 
 //TestOsExit -退出当前程序
@@ -52,17 +69,13 @@ func TestOsExit(t *testing.T){
 
 //TestOsStdErr -错误输出到控制台
 func TestOsStdErr(t *testing.T){
+	//os.Stderr io.Writer
 	err := errors.New("请求错误")
 	fmt.Fprint(os.Stderr,err)
 }
 
 
-//TestOsHostName - 返回主机名
-func TestOsHostName(t *testing.T){
-	hostname,err := os.Hostname()
-	fmt.Println(hostname)
-	fmt.Println(err)
-}
+
 
 
 
