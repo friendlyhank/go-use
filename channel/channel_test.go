80c06fd9d9d53b72a792c403c8aaf177e9f1e648
a853160447 <hanguang8888@outlook.com>
2019-07-03 07:59:49 +0800

    channel

--- channel/channel_test.go
+++ channel/channel_test.go
@@ -1,6 +1,7 @@
 package channel
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -11,6 +12,7 @@ func TestChannelMap(t *testing.T){
 
 	syncChan := make(chan struct{}, 2)
 	go func() { // 用于演示接收操作。
+		//不多接收值需要for循环
 		for {
 			if elem, ok := <-mapChan; ok {
 				elem["count"]++
@@ -33,4 +35,43 @@ func TestChannelMap(t *testing.T){
 	}()
 	<-syncChan
 	<-syncChan
+}
+
+//一个接收信号触发接收得例子
+func TestChannelSyncMap(t *testing.T){
+	var strChan = make(chan string, 3)
+
+	syncChan1 := make(chan struct{}, 1)
+	syncChan2 := make(chan struct{}, 2)
+	go func() { // 用于演示接收操作。
+		//没有for可以接收单个值，因为没有值是阻塞状态，可以作为触发装置控制
+		<-syncChan1
+		fmt.Println("Received a sync signal and wait a second... [receiver]")
+		time.Sleep(time.Second)
+		for {
+			if elem, ok := <-strChan; ok {
+				fmt.Println("Received:", elem, "[receiver]")
+			} else {
+				break
+			}
+		}
+		fmt.Println("Stopped. [receiver]")
+		syncChan2 <- struct{}{}
+	}()
+	go func() { // 用于演示发送操作。
+		for _, elem := range []string{"a", "b", "c", "d"} {
+			strChan <- elem
+			fmt.Println("Sent:", elem, "[sender]")
+			if elem == "c" {
+				syncChan1 <- struct{}{}
+				fmt.Println("Sent a sync signal. [sender]")
+			}
+		}
+		fmt.Println("Wait 2 seconds... [sender]")
+		time.Sleep(time.Second * 2)
+		close(strChan)
+		syncChan2 <- struct{}{}
+	}()
+	<-syncChan2
+	<-syncChan2
 }
