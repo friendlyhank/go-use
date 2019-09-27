324fe36afcfdba480b4352de60c44b8fb837a6ea
Hank <853160447@qq.com>
2019-09-27 11:50:00 +0800

    copy or del slice

---
+++ slice/slice_test.go
@@ -0,0 +1,28 @@
+package slice
+
+import (
+	"testing"
+)
+
+//Slice复制
+func TestSliceCopy(t *testing.T){
+	enport :=[]string{"2379","2380"}
+	eps := make([]string, len(enport))
+	copy(eps,enport)
+
+	t.Logf("%v",eps)
+}
+
+func DeleteSlice(a []int,num int) []int{
+	for i := 0; i < len(a); i++ {
+		if a[i] == num {
+			a = append(a[:i], a[i+1:]...)
+		}
+	}
+	return a
+}
+//删除
+func TestSliceDel(t *testing.T){
+	eps := DeleteSlice([]int{2379,2380},2380)
+	t.Logf("%v",eps)
+}
