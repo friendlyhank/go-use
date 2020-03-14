761b78142a9477a376d3096349244e9bf8201685
Hank <853160447@qq.com>
2020-03-14 14:07:01 +0800

    map range delete

--- map/map_test.go
+++ map/map_test.go
@@ -6,23 +6,74 @@ import (
 
 type MapGoods map[string]string
 /*
-*map定义和初始化
+	*map定义和初始化
 
-#定义map m
-var m map[string]int  //nil
+	#定义map m
+	var m map[string]int  //nil
 
-#初始化方式1 make
-m = make(map[string]int)
+	#初始化方式1 make
+	m = make(map[string]int)
 
-#初始化方式2 花括号
-m = map[string]int{}
+	#初始化方式2 花括号
+	m = map[string]int{}
 
-#也可以直接赋值
-m = map[string]int{
-		"Tom":50,
-		"Mary":60,
-	}
+	#也可以直接赋值
+	m = map[string]int{
+			"Tom":50,
+			"Mary":60,
+		}
+/*
+
+/*
+ *
  */
+func TestMapBool(t *testing.T){
+	mapScore :=map[string]int{
+		"Tom":78,
+		"Mary":60,
+		"Kevin":90,
+	}
+
+	//
+	score,ok := mapScore["Tom"]
+	if ok{
+		t.Logf("Name:Tom,Score:%d",score)
+	}
+
+	_,exist := mapScore["Tom"]
+	if exist{
+		t.Logf("这个值是存在滴!")
+	}
+}
+
+/**
+ *Map的遍历以及无序性
+ */
+func TestMapRange(t *testing.T){
+	mapScore :=map[string]int{
+		"Tom":78,
+		"Mary":60,
+		"Kevin":90,
+	}
+
+	for name,score := range mapScore{
+		t.Logf("Name:%s,Score:%d",name,score)
+	}
+	//多次运行结果可以看出map的输出是无序
+}
+
+/*
+ *delete删除
+ */
+func TestMapDelete(t *testing.T){
+	mapScore := make(map[string]int)
+
+	mapScore["Mary"] = 60
+	t.Logf("Mary score:%d",mapScore["Mary"])
+
+	delete(mapScore,"Mary")
+	t.Logf("delete Mary:%d",mapScore["Mary"])
+}
 
 //TestQuoteMap- map是引用类型案例1
 func TestQuoteMap(t *testing.T){
