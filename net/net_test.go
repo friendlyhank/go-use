2cf93a3d2b8c64413fe70f4dd651c3f8554e3ff7
hank <hanguang8888@outlook.com>
2024-04-05 20:10:16 +0800

    拼接host和prot

--- net/net_test.go
+++ net/net_test.go
@@ -7,114 +7,117 @@ import (
 	"time"
 )
 
-//=============================================net socket=======================================
+// =============================================net socket=======================================
+// TestJoinHostPort - 拼接主机和端口
+func TestJoinHostPort(t *testing.T) {
+	t.Logf("%v", net.JoinHostPort("127.0.0.1", "8080"))
+}
 
 /**
 *read write  listen.Accept不会保持阻塞的风格，所以要自己建立阻塞
-*/
+ */
 
-//TestNetListen -设置本地监听端口
-func TestNetListen(t *testing.T){
+// TestNetListen -设置本地监听端口
+func TestNetListen(t *testing.T) {
 	ln, err := net.Listen("tcp", ":8080")
 	if err != nil {
 		// handle error
-		t.Errorf("%v",err)
+		t.Errorf("%v", err)
 	}
 
 	//for{
-		//接收每个链接
-		conn,err := ln.Accept()
-		if err != nil{
-			//handle error
-			t.Errorf("%v",err)
-			return
-		}
-		t.Logf("%v",conn)
+	//接收每个链接
+	conn, err := ln.Accept()
+	if err != nil {
+		//handle error
+		t.Errorf("%v", err)
+		return
+	}
+	t.Logf("%v", conn)
 
-		//输出地址listen.Addr
-		t.Logf("%v",ln.Addr())
+	//输出地址listen.Addr
+	t.Logf("%v", ln.Addr())
 
-		//
-		t.Logf("%v",conn.RemoteAddr())
+	//
+	t.Logf("%v", conn.RemoteAddr())
 	//}
 }
 
-//TestNetDial -Dial拨号功能连接服务器
-func TestNetDial(t *testing.T){
-	conn,err := net.Dial("tcp","127.0.0.1:8080")
+// TestNetDial -Dial拨号功能连接服务器
+func TestNetDial(t *testing.T) {
+	conn, err := net.Dial("tcp", "127.0.0.1:8080")
 	status, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil{
-		t.Errorf("%v",err)
+	if err != nil {
+		t.Errorf("%v", err)
 		return
 	}
-	t.Logf("%v",status)
+	t.Logf("%v", status)
 }
 
-//TestNetDialTimeOut -Dial拨号功能连接服务器
-func TestNetDialTimeOut(t *testing.T){
+// TestNetDialTimeOut -Dial拨号功能连接服务器
+func TestNetDialTimeOut(t *testing.T) {
 	//1秒的超时呼叫
-	conn,err := net.DialTimeout("tcp","127.0.0.1:8080",time.Second)
+	conn, err := net.DialTimeout("tcp", "127.0.0.1:8080", time.Second)
 	status, err := bufio.NewReader(conn).ReadString('\n')
-	if err != nil{
-		t.Errorf("%v",err)
+	if err != nil {
+		t.Errorf("%v", err)
 		return
 	}
-	t.Logf("%v",status)
+	t.Logf("%v", status)
 }
 
-//获取IP地址
-func TestInterfaceAddrs(t *testing.T){
-	addrs,_ := net.InterfaceAddrs()
+// 获取IP地址
+func TestInterfaceAddrs(t *testing.T) {
+	addrs, _ := net.InterfaceAddrs()
 
-	for _,address := range addrs{
+	for _, address := range addrs {
 		ipnet, ok := address.(*net.IPNet)
 		if !ok {
 			continue
 		}
 
 		isUnspecified := ipnet.IP.IsUnspecified()
-		if isUnspecified{
-			t.Logf("%v","isUnspecified")
-			t.Logf("地址%v",ipnet.IP.String())
+		if isUnspecified {
+			t.Logf("%v", "isUnspecified")
+			t.Logf("地址%v", ipnet.IP.String())
 		}
 
 		//是否环回地址
-		isLoopback :=ipnet.IP.IsLoopback()
-		if isLoopback{
-			t.Logf("%v","isLoopback")
-			t.Logf("回环地址%v",ipnet.IP.String())
+		isLoopback := ipnet.IP.IsLoopback()
+		if isLoopback {
+			t.Logf("%v", "isLoopback")
+			t.Logf("回环地址%v", ipnet.IP.String())
 		}
 
 		//多播地址
 		isMulticast := ipnet.IP.IsMulticast()
-		if isMulticast{
-			t.Logf("%v","isMulticast")
-			t.Logf("地址%v",ipnet.IP.String())
+		if isMulticast {
+			t.Logf("%v", "isMulticast")
+			t.Logf("地址%v", ipnet.IP.String())
 		}
 
 		isInterfaceLocalMulticast := ipnet.IP.IsInterfaceLocalMulticast()
-		if isInterfaceLocalMulticast{
-			t.Logf("%v","isInterfaceLocalMulticast")
+		if isInterfaceLocalMulticast {
+			t.Logf("%v", "isInterfaceLocalMulticast")
 		}
 
 		isLinkLocalMulticast := ipnet.IP.IsLinkLocalMulticast()
-		if isLinkLocalMulticast{
-			t.Logf("%v","isLinkLocalMulticast")
-			t.Logf("地址%v",ipnet.IP.String())
+		if isLinkLocalMulticast {
+			t.Logf("%v", "isLinkLocalMulticast")
+			t.Logf("地址%v", ipnet.IP.String())
 		}
 
 		isLinkLocalUnicast := ipnet.IP.IsLinkLocalUnicast()
-		if isLinkLocalUnicast{
-			t.Logf("%v","isLinkLocalUnicast")
-			strIp :=ipnet.IP.String()
-			t.Logf("地址%v",strIp)
+		if isLinkLocalUnicast {
+			t.Logf("%v", "isLinkLocalUnicast")
+			strIp := ipnet.IP.String()
+			t.Logf("地址%v", strIp)
 		}
 		isGlobalUnicast := ipnet.IP.IsGlobalUnicast()
-		if isGlobalUnicast{
-			t.Logf("%v","isGlobalUnicast")
-			t.Logf("地址%v",ipnet.IP.String())
+		if isGlobalUnicast {
+			t.Logf("%v", "isGlobalUnicast")
+			t.Logf("地址%v", ipnet.IP.String())
 		}
 
 	}
 }
-
