22a9468bb0157cdf684b298a07ff23986e2ef442
hank <hanguang8888@outlook.com>
2025-06-29 17:40:23 +0800

    flag

--- flag/flag.go
+++ flag/flag.go
@@ -5,11 +5,8 @@ import (
 	"fmt"
 	"github.com/astaxie/beego/logs"
 	"os"
+	"strings"
 )
-
-func main(){
-	FlagSetParse()
-}
 
 var (
 	flagHelp = `
@@ -19,30 +16,30 @@ Member:
    `
 )
 
-func FlagSetParse(){
-	/**
-	1.flag自带help命令
-	go run flag.go --help
+/*
+*
+1.flag自带help命令
+go run flag.go --help
 
-	2.也支持自定义NewFlagSet etcd
-	go run flag.go --etcd
+2.也支持自定义NewFlagSet etcd
+go run flag.go --etcd
 
-	3.errorHandling有三个
-	flag.ContinueOnError //错误但是会继续执行程序
-	ExitOnError //错误然后终止程序
-	PanicOnError //错误会抛出异常
-
-	 */
-	 //1.NewFlagSet new一个指令
-	fs := flag.NewFlagSet("etcd",flag.ContinueOnError)
+3.errorHandling有三个
+flag.ContinueOnError //错误但是会继续执行程序
+ExitOnError //错误然后终止程序
+PanicOnError //错误会抛出异常
+*/
+func NewFlagSet() {
+	//1.NewFlagSet new一个指令
+	fs := flag.NewFlagSet("etcd", flag.ContinueOnError)
 
 	//2.设置指令
-	fs.String("config","","path to config file")
-	fs.String("log-prefix","[nsqlookupd]","path to config file")
-	fs.Bool("version",false,"path to version")
+	fs.String("config", "", "path to config file")
+	fs.String("log-prefix", "[nsqlookupd]", "path to config file")
+	fs.Bool("version", false, "path to version")
 	err := fs.Parse(os.Args[1:])
 
-	logs.Info("%v",err)
+	logs.Info("%v", err)
 
 	//3.Help指令
 	if flag.ErrHelp == err {
@@ -50,25 +47,45 @@ func FlagSetParse(){
 	}
 
 	/**
-		当只有输入--help或者添加的指令--etcd时,len(fs.Args()) == 0
-		如果输入其他指令者不为零,此处可以检测无法识别的输入指令(除了注册的指令和系统自带指令)
-	 */
-	if(len(fs.Args()) != 0){
-		logs.Info("%v","无法识别指令")
+	当只有输入--help或者添加的指令--etcd时,len(fs.Args()) == 0
+	如果输入其他指令者不为零,此处可以检测无法识别的输入指令(除了注册的指令和系统自带指令)
+	*/
+	if len(fs.Args()) != 0 {
+		logs.Info("%v", "无法识别指令")
 	}
 
 	//4.输出某个具体的指令
 	fmt.Println(fs.Arg(0))
 
-	//5.指定命令输出
-	/**
-		Lookup查询具体某个指令,flag使用map去查询的
-		里面接口的实现比较难理解，以后总结Hank
-	 */
-	if fs.Lookup("version").Value.(flag.Getter).Get().(bool){
-		fmt.Println("nsqlookupd2.0")
-		os.Exit(0)
-	}
+	//5.获取指定命令的输出
+	fs.Lookup("version").Value.String()
 
+	isVersion := fs.Lookup("version").Value.(flag.Getter).Get().(bool)
+	fmt.Println(isVersion)
 }
 
+// 定义自定义类型
+type StringSlice []string
+
+// 实现 Value.String() 方法
+func (s *StringSlice) String() string {
+	return fmt.Sprintf("%v", *s)
+}
+
+// 实现 Value.Set() 方法
+func (s *StringSlice) Set(value string) error {
+	*s = strings.Split(value, ",")
+	return nil
+}
+
+// FlagVar - 自定义复杂类型的命令解析方式
+func FlagVar() {
+	var list StringSlice
+	flag.Var(&list, "list", "Comma-separated list of values")
+
+	// 解析参数
+	flag.Parse()
+
+	// 输出结果
+	fmt.Println("Parsed list:", list)
+}
--- flag/flag_test.go
+++ flag/flag_test.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"flag"
-	"github.com/astaxie/beego/logs"
 	"os"
 	"testing"
 )
 
-func TestFlagParse(t *testing.T) {
+// TestNewFlagSet - 初始化新的指令解析
+func TestNewFlagSet(t *testing.T) {
+	NewFlagSet()
+}
 
+// TestVar - 自定义的复杂的命令规则
+func TestVar(t *testing.T) {
+	os.Args = []string{"myprogram", "--list", "a,b,c"}
+	FlagVar()
 }
