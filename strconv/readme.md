ef1924e407a41be60403f3fa9114f0ec12c19e82
hank <hanguang8888@outlook.com>
2025-07-06 21:34:34 +0800

    strconv

--- strconv/readme.md
+++ strconv/readme.md
@@ -1,5 +1,7 @@
 package strconv
 
-
 # 将字符串如true解析bool类型
 ParseBool(str string) (bool, error)
+
+# 将bool类型转换为字符串
+func FormatBool(b bool) string 
--- strconv/strconv_test.go
+++ strconv/strconv_test.go
@@ -15,3 +15,11 @@ func Test_ParseBool(t *testing.T) {
 	}
 	fmt.Println(v)
 }
+
+// 返回字符串字面量和转义字符
+func Test_Quote(t *testing.T) {
+	// 返回一个用双引号括起的 Go 字符串字面量，并返回对应go的转义序列(\t, \n, \xFF, \u0100)
+	fmt.Println(strconv.Quote(`Hello
+World`))
+	fmt.Println("Hello\nWorld")
+}
